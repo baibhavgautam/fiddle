8feb4d60572942b73f8b1f95ab86ba7dfba23c2a
baibhavgautam <baibhavgautam@jaguardesignstudio.com>
2018-01-10 20:13:09 -0800

    Add minor jquery animations

--- comment-ui-updated/script.js
+++ comment-ui-updated/script.js
@@ -1,8 +1,15 @@
-$('#comment-toggle-btn').click(function(e) {
+const performDelayedAction = action => setTimeout(() => action(), 200)
+
+const adjustWidth = () => $('#page-content').toggleClass('col-sm-12 col-sm-9')
+
+const toggleCommentSidebar = () => $('#comment-sidebar').fadeToggle()
+
+let count = 0
+
+$('#comment-toggle-btn').click(e => {
   e.preventDefault()
-  // $('#page-content').html('what?')
-  // $(this).toggleClass('flip')
-  $('#page-content').toggleClass('col-sm-12')
-  $('#page-content').toggleClass('col-sm-9')
-  $('#comment-sidebar').toggle()
+  count++
+  (count % 2 === 0)
+  ? toggleCommentSidebar() && performDelayedAction(adjustWidth)
+  : adjustWidth() && performDelayedAction(toggleCommentSidebar)
 })
